bba9ddbeb15278013a002f4df9a4b0104da39f9b
ryanm <ryan.mason95@hotmail.co.uk>
2023-02-02 23:44:24 +0000

    Fixed audio context autostart error + refactoring

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
+button.addEventListener("click", function(){
+    const audioControls = document.querySelector("#myAudio");
+    audioControls.style.visibility = "inherit";
+    const sliderControls = document.querySelector("#slider");
+    sliderControls.style.visibility = "inherit";
+    analyser = init();
+    const button = document.querySelector("#button");
+    button.style.display = "none";
+    button.disabled = true;
+})
 
+// Set number of bars
+const numberOfBars = 75;
 
-const audio = document.querySelector("audio");
-const audioContext = new AudioContext();
-const numberOfBars = 100;
-const audioSource = audioContext.createMediaElementSource(audio);
-const analyser = audioContext.createAnalyser();
-let smoothingConstant = 0;
-function updateSmoothness(smoothness){
-    smoothingConstant = smoothness;
-};
-
-audioSource.connect(analyser);
-audioSource.connect(audioContext.destination);
-
-const frequencyData = new Uint8Array(analyser.frequencyBinCount);
-analyser.getByteFrequencyData(frequencyData);
-console.log("frequencyData", frequencyData);
-
+// Get visualiser container
 const visualiserContainer = document.querySelector(".visualiser-container");
 
+// Create bars
 for( let i = 0; i < numberOfBars; i++) {
     const bar = document.createElement("DIV");
     bar.setAttribute("id", "bar" + i );
@@ -26,28 +23,50 @@ for( let i = 0; i < numberOfBars; i++) {
     visualiserContainer.appendChild(bar);
 }
 
+let smoothingConstant = 0;
+function updateSmoothness(smoothness){
+    smoothingConstant = smoothness;
+}; 
 
+function init(){
+    console.log("init");
 
+    // Get audio element
+    let audio = document.querySelector("audio");
+    
+    // Create audio context
+    let audioContext = new AudioContext();
 
-
-renderFrame();
-function renderFrame() {
-    analyser.getByteFrequencyData(frequencyData);
-
-    for( let i = 0; i< numberOfBars; i++) {
-        const data = frequencyData[i];
-        const bar = document.querySelector("#bar" + i);
-        if(!bar){
-            continue;
+    // Create audio source
+    let audioSource = audioContext.createMediaElementSource(audio);
+    
+    // Create audio analyser
+    let analyser = audioContext.createAnalyser();
+    
+    // Connect source node to analyser
+    audioSource.connect(analyser);
+    audioSource.connect(audioContext.destination);
+    
+    function renderFrame() {        
+        // Create frequency data array
+        let frequencyData = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(frequencyData);
+        for( let i = 0; i < numberOfBars; i++) {
+            const data = frequencyData[i];
+            const bar = document.querySelector("#bar" + i);
+            if(!bar){
+                continue;
+            }
+            let barHeight = (Math.max( data * 5 ) + bar.offsetHeight) / 2;
+            if( i != 0 && i < numberOfBars - 2){
+                const previousBar = document.querySelector("#bar" + (i-1));
+                const nextBar = document.querySelector("#bar" + (i+1));
+                barHeight = ((previousBar.offsetHeight + nextBar.offsetHeight) / 2) * smoothingConstant + barHeight * (1 - smoothingConstant); 
+            }
+            bar.style.height = barHeight + "px";
+            bar.style.backgroundColor = `hsl(${barHeight}, 50%, 50%)`;
         }
-        let barHeight = (Math.max( data * 5 ) + bar.offsetHeight) / 2;
-        if( i != 0 && i < 99){
-            const previousBar = document.querySelector("#bar" + (i-1));
-            const nextBar = document.querySelector("#bar" + (i+1));
-            barHeight = ((previousBar.offsetHeight + nextBar.offsetHeight) / 2) * smoothingConstant + barHeight * (1 - smoothingConstant); 
-        }
-        bar.style.height = barHeight + "px";
-        bar.style.backgroundColor = `hsl(${barHeight}, 50%, 50%)`;
-    }
-    window.requestAnimationFrame(renderFrame);
+        window.requestAnimationFrame(renderFrame);
+    };
+    renderFrame();
 };
